61d2599fe5f242f902fc3a7616b7e5e6c8b5cbdf
jf <jf@taocz.cn>
2014-12-29 15:31:46 +0800

    Signed-off-by: jf <jf@taocz.cn>

---
+++ 1.js
@@ -0,0 +1,17 @@
+&lt;/div&gt;
+<div class="yh-cont">        
+	<div class="k1200">            
+		<img src="{$top_img}" border="0" usemap="#Map" class="dsb" alt="" />
+		<map name="Map" id="Map">
+			<area shape="rect" coords="37,213,355,478" href="http://yihao.taocz.com/store-1235-39.html" target="_blank" />
+			<area shape="rect" coords="354,180,841,478" href="http://mdllx.taocz.com/store-1279-39.html" target="_blank" />
+			<area shape="rect" coords="354,180,841,478" href="http://mdllx.taocz.com/store-1279-39.html" target="_blank" />
+			<area shape="rect" coords="841,217,1160,478" href="http://www.taocz.com/store-1176-46.html?num=1&amp;domain=yintai.taocz.com" target="_blank" />
+			<area shape="rect" coords="67,525,430,715" href="http://weibo.com/1960233904/profile?topnav=1&amp;wvr=5&amp;from=company&amp;user=1" target="_blank" />
+		</map>          
+	</div>        
+</div>
+
+
+
+<script>function a(id,r){$.post("http://www.taocz.com/index.php?app=specact&act=check_hyzx_convert",{rmb:r},function(mesg){if(mesg.code==-1){alert("请先登入！");}else if(mesg.code==-3){alert("不满领取本券所需条件,无权领取！");$(id).off("click");}else if(mesg.code==-2){alert("您已经领取过本券，不能再次领取！");$(id).off("click");}else if(mesg.code==1){alert("领取购物券成功！请至会员中心查收！");$(id).off("click");}else{return false;}},"json");}$("#new_mem").click(function(){a('#new_mem',5);return false;});$("#old_mem").click(function(){a('#old_mem',10);return false;});</script>
---
+++ p/WxPay.config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* 
+*/
+
+define(APPID , "wx11ee013f1fe0d4f7");  //appid
+define(APPKEY ,"OsLHsxPdri97KTdzK2exBfOOBe302xgJmNtKgMTHDTmJezI5CJLA9AMjzOHdWWa7lwQpszgvlNPkNjuwh3E1mIqDfXF2yBv3FzMjoVNg9aIGcctns63EQnMLZ2e9RwYG"); //paysign key
+define(SIGNTYPE, "sha1"); //method
+define(PARTNERKEY,"");//通加密串
+define(APPSERCERT, "c7e197cb050c9841a3a806758d2d1f83");
+
+?>
