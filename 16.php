5a85868dc2121d69bbc10d67007b0ee336d0ef8d
jf <jf@taocz.cn>
2014-12-30 09:37:41 +0800

    77

--- 16.php
+++ 16.php
@@ -1,15 +1,15 @@
 <?
 
-$sn = '141220164756334';
+$sn = '141229094185274';
 $commonCode = "NHCJ".rand(1000000,9999999).rand(100000,999999);
 						$strSQL="INSERT INTO tcz_coupon_sn(coupon_id,user_id,begintime,endtime,commoncode,money,minprice,issue,ischecked,createtime)
-								VALUE(1376,'114833','".date("Y-m-d H:i:s")."','".date("Y-m-d H:i:s", (time()+86400*60))."','".$commonCode."',50,100,1,0,now())";
+								VALUE(1376,'885272','".date("Y-m-d H:i:s")."','".date("Y-m-d H:i:s", (time()+86400*60))."','".$commonCode."',50,100,1,0,now())";
 				
 echo $strSQL , '<br>';
 
 
 
-$sql = "insert into tcz_specact_luck_draw (name,order_sn_main,uid,specact_tag,beizhu, level) values ('{$commonCode}', '{$sn}', '114833', 'nhcj', '100', '50')";
+$sql = "insert into tcz_specact_luck_draw (name,order_sn_main,uid,specact_tag,beizhu, level) values ('{$commonCode}', '{$sn}', '885272', 'nhcj', '100', '50')";
 echo $sql;		
 
 
